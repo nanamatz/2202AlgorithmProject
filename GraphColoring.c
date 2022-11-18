936a646e39be598f482160f20e892357960c069d
Karim <2jh0926@naver.com>
2022-11-19 01:16:22 +0900

    GraphColoring 게임화

--- GraphColoring.c
+++ GraphColoring.c
@@ -1,4 +1,6 @@
 ﻿#include <stdio.h>
+#include <windows.h>
+
 #define NODE_NUM_1 6
 #define NODE_NUM_2 7
 #define NODE_NUM 12
@@ -36,7 +38,6 @@ int connectionMatrix_3[NODE_NUM][NODE_NUM] = {
 	{1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1},
 	{0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0}
 };
-
 /// <summary>
 /// node_color 배열에 color를 칠한다.
 /// </summary>
@@ -64,18 +65,76 @@ int GraphColoring(int* node_color, int node_index, int color) {
 
 
 int main() {
+	int success = 1;
 
-
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	printf("$                             $$                                     $$                            $\n");
+	printf("$                             $$                                     $$                            $\n");
+	printf("$                             ,.                                     !.                            $\n");
+	printf("$           1                 ,.                            $$$$$$$$$-$$$$$$$$$~                   $\n");
+	printf("$                       ------:~---,                        $                  *                   $\n");
+	printf("$                      -:          *                        $                  *                   $\n");
+	printf("$                      -:          *                        $                  *                   $\n");
+	printf("$    !$$$$$$$$$$$;     -:          *                        $                  *                   $\n");
+	printf("$    *          .*,,,,,::          *       7                $          10      *                   $\n");
+	printf("$    *          .$;;;;;*:    6     *                        $                  *         12        $\n");
+	printf("$    *          .!     -:          *                        $                  *                   $\n");
+	printf(";    *          .!     -:          *                        $                  *                   $\n");
+	printf(";    *          .!     -:          *                        $                  *                   $\n");
+	printf(";    *    2     .!      $$$$$$$$$$$$                        $                  *                   $\n");
+	printf(";    *          .!            ,.                            $........,.........!                   $\n");
+	printf(";    *          .!            ,.                                     !.                            $\n");
+	printf(";    *          .!            ,.                         *$$$$$$$$$$$@#$$$$$$$$$$!                 $\n");
+	printf(";    *          .!            ,.            .,,,,,,,,,,  $                       $                 $\n");
+	printf(";    *          .!            ,.            $;;;;;;;;;!  $                       $                 $\n");
+	printf("$    $$$$$$$$$$$$$            $$            $         $  $                       $                 $\n");
+	printf("$ 1  $   $      $$            $$$$$$$$$$$$$$$         $$$$           9           $$$$$$$$$$$$$$$$$$$\n");
+	printf("$    $   @      .!                          $         ,  $                       $                 #\n");
+	printf("$    $   @      .!                          $         ,  $                       $                 $\n");
+	printf(";    $   @      .!                          $         ,  *$$$$$$$$$$$$$$$$$$$$$$$!                 $\n");
+	printf(";    $   @      .!                          $         ,                                            $\n");
+	printf(";    $   @      .!        5                 $         ,                                            $\n");
+	printf(";    $ 3 @   4  .!                          $    8    ,                                            $\n");
+	printf(";    $   @      .!                          $         ,                                            $\n");
+	printf(";    $   @      .!                          $         ,                                            $\n");
+	printf(";    $   @      .!                          $         ,                                            $\n");
+	printf(";    $   @$$$$$$.!$$$$$$$$$$$$$$$$$$$$$$$$$$$         ,                                            $\n");
+	printf(";    $          .!                          $         ,                                            $\n");
+	printf(";    $,,,,,,,,,,-!                          $         ,                                            $\n");
+	printf(";    $;;;;;;;;;;;-                          $         ,                                            $\n");
+	printf(";                                           $$$$$$$$$$#                 11                         $\n");
+	printf(";                        1                      @                                                  $\n");
+	printf(";                                               @                                                  $\n");
+	printf(";                                               @                                                  $\n");
+	printf(";                                               @                                                  $\n");
+	printf(";                                               @                                                  $\n");
+	printf(";:::::::::::::::::::::::::::::::::::::::::::::::!::::::::::::::::::::::::::::::::::::::::::::::::::!\n");
 
 	// 각 그래프 노드가 가질 color 값, default = -1;
 	int node_color[12];
-	for (int i = 0; i < 12; i++)
+	int input_color[12];
+	for (int i = 0; i < 12; i++) {
+		printf("%d번 칸에 색칠할 색깔(0, 1, 2, 3, 4 ...)을 입력해주세요. (단, 1번 칸에는 0을, 2번 칸에는 1을 칠해주세요.)\n", i + 1);
+		printf(">> ");
+		scanf_s(" %d", &input_color[i]);
 		node_color[i] = -1;
+	}
 
 	GraphColoring(node_color, 0, 0);
+
 	for (int i = 0; i < NODE_NUM; i++) {
-		printf("%d node : %d color\n", i, node_color[i]);
+		printf("%d번 칸에 색칠해야되는 색은 %d색이었습니다.\n", i + 1, node_color[i]);
+		Sleep(5);
+		printf("%d번 칸에 %d색을 칠하셨습니다.\n", i + 1, input_color[i]);
+		Sleep(5);
+		if (node_color[i] != input_color[i])
+			success = 0;
 	}
+
+	if (success == 1)
+		printf("축하드립니다! 정답입니다!");
+	else
+		printf("아쉽지만 오답입니다. 다시 생각해보세요.");
 
 
 	return 0;
